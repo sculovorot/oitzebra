144362c888a4bd9a70a7b953b33076e348a45df2
Aleksey <sculovorot@mail.ru>
2020-03-18 15:15:34 +0300

    добавил js для входа на главную

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let login_ok = false,
+    user_name = "",
+    password = "";
+function Input() {
+    user_name = "";
+    password = "";
+    user_name = prompt('Введите логин');    
+    password = prompt('Введите пароль'); 
+    if ( user_name == "admin" && password == "password" ) {
+        login_ok = true;    
+    }
+    if ( login_ok == false || user_name == "" || password == "") {
+        alert ( 'введен неверный логин или пароль');
+        Input();
+    }
+}
+Input();
