9e680c661afda6ffcc97ab7000f85ee3ef8984ed
Aleksey <sculovorot@mail.ru>
2020-03-18 16:19:56 +0300

    добавил страницу входа

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function Input() {
     user_name = prompt('Введите логин');    
     password = prompt('Введите пароль'); 
     if ( user_name == "admin" && password == "password" ) {
-        login_ok = true;    
+        login_ok = true; 
+        window.location = 'index.html';  
     }
     if ( login_ok == false || user_name == "" || password == "") {
         alert ( 'введен неверный логин или пароль');
